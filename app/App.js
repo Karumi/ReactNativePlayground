48aea07f71b82aada082747a098f78f0b32390fb
Pedro Vicente Gómez Sánchez <pedrovicente.gomez@gmail.com>
2018-10-10 16:52:07 +0200

    Add crash reporter to our project

--- app/App.js
+++ app/App.js
@@ -1,3 +1,6 @@
-import App from './src/App'
+import App from './src/App';
+import Sentry from 'sentry-expo';
 
-export default App
+Sentry.config('https://b2ba5e1a6cbf4ff79a89291db6ecc3fb@sentry.io/1298369').install();
+
+export default App;
