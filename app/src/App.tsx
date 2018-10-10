c269fb50f5fc638b3d328b3ceb27089af8dbf39d
Pedro Vicente Gómez Sánchez <pedrovicente.gomez@gmail.com>
2018-10-10 15:00:17 +0200

    Add tslint support

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,23 +1,23 @@
-import * as React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class App extends React.Component {
-  render () {
+  public render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app usign Typescript and Jest!</Text>
       </View>
-    )
+    );
   }
 }
 
-const defaultBackgroundColor = '#FFF'
+const defaultBackgroundColor = "#FFF";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: defaultBackgroundColor,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- app/src/__tests__/App.test.tsx
+++ app/src/__tests__/App.test.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react'
-import * as renderer from 'react-test-renderer';
+import * as React from "react";
+import * as renderer from "react-test-renderer";
 
-import App from '../App'
+import App from "../App";
 
-it('renders correctly with defaults', () => {
-    const app = renderer.create(<App />).toJSON()
-    expect(app).toMatchSnapshot()
-})
+it("renders correctly with defaults", () => {
+    const app = renderer.create(<App />).toJSON();
+    expect(app).toMatchSnapshot();
+});
