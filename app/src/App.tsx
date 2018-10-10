27a052ddcdb7dadd981434aa0636818992bd862a
Pedro Vicente Gómez Sánchez <pedrovicente.gomez@gmail.com>
2018-10-10 14:43:06 +0200

    Get the tests passing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import * as React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 export default class App extends React.Component {
   render () {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app usign Typescript!</Text>
+        <Text>Open up App.js to start working on your app usign Typescript and Jest!</Text>
       </View>
     )
   }
