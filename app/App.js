730a89d93649dfebd23f813fa792fdee9223e5f4
Pedro Vicente Gómez Sánchez <pedrovicente.gomez@gmail.com>
2018-10-11 11:25:24 +0200

    Get native base working

--- app/App.js
+++ app/App.js
@@ -1,6 +1,40 @@
-import App from './src/App';
-import Sentry from 'sentry-expo';
+import React from "react";
+import App from "./src/App";
+import Sentry from "sentry-expo";
+import Expo from "expo";
 
-Sentry.config('https://b2ba5e1a6cbf4ff79a89291db6ecc3fb@sentry.io/1298369').install();
+class ReactNativePlaygroundApp extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { loading: true };
+    }
 
-export default App;
+    async componentWillMount() {
+        this.initializeSentry();
+        await this.initializeNativeBaseFonts();
+        this.setState({ loading: false });
+    }
+
+    render() {
+        if (this.state.loading) {
+            return <Expo.AppLoading />;
+        } else {
+            return (<App />);
+        }
+
+    }
+
+    initializeSentry() {
+        Sentry.config('https://b2ba5e1a6cbf4ff79a89291db6ecc3fb@sentry.io/1298369').install();
+    }
+
+    initializeNativeBaseFonts() {
+        return Expo.Font.loadAsync({
+            'Roboto': require('native-base/Fonts/Roboto.ttf'),
+            'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+            'Ionicons': require('@expo/vector-icons/fonts/Ionicons.ttf'),
+        });
+    }
+}
+
+export default ReactNativePlaygroundApp;
