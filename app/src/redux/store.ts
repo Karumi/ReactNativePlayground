75061caa2576ab3c9465807a27b045187c2306ab
Pedro Vicente Gómez Sánchez <pedrovicente.gomez@gmail.com>
2018-10-17 16:03:56 +0200

    Explain the redux dev tool integration

--- app/src/redux/store.ts
+++ app/src/redux/store.ts
@@ -1,6 +1,7 @@
 
 import { RouterAction } from "react-router-redux";
 import { combineReducers, createStore } from "redux";
+import devToolsEnhancer from "remote-redux-devtools";
 import { StateType } from "typesafe-actions";
 import { CountersAction } from "../react-redux/counters";
 import countersReducer from "../react-redux/counters";
@@ -13,6 +14,6 @@ const rootReducer = combineReducers({
 });
 
 function configureStore(initialState?: object) {
-    return createStore(rootReducer, initialState!);
+    return createStore(rootReducer, initialState!, devToolsEnhancer({ realtime: true }));
 }
 export default configureStore();
